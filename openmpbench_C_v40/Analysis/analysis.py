c3d11cf0124b1c48b3b4ed08d688aa84e516a6ca
Weiyu Tu <s1820742@ed.ac.uk>
2024-04-09 15:09:39 +0100

    Analyze distribution of 100 runs using linearly spaced sampling
    Implemented an analysis method to explore the distribution characteristics of 100 simulation runs. Sampling was performed with linear spacing to ensure even coverage across the entire dataset. This approach helps in understanding the underlying trends and variations more accurately. Adjustments were made to the analysis pipeline to accommodate the linear sampling method, enhancing the reliability of the findings.

---
+++ openmpbench_C_v40/Analysis/analysis.py
@@ -0,0 +1,108 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import zscore
+import argparse
+
+
+def runs_dist(data_content, job, filename):
+    # Plot histograms for each column
+    fig, ax = plt.subplots(figsize=(20, 10))
+    n_bins = 50  # Adjust the number of bins for finer granularity
+    max_height = 0  # To store the max height of histograms for positioning text
+    all_means = []
+    for i, column in enumerate(data_content):
+        counts, bins, patches = ax.hist(column, bins=n_bins, density=True, alpha=0.5, label=f'#{i+1}')
+        tip = max(counts)
+        if (tip > max_height):
+            max_height = tip
+        mean = np.mean(column)
+        all_means.append(mean)
+        median = np.median(column)
+        std_dev = np.std(column)
+        stats_text = f"#{i+1}:\nMean {mean:.2f}\nMedian {median:.2f}\nStd {std_dev:.2f}"
+        print
+        # Position the stats text beside each histogram
+        x_pos = mean + std_dev
+        y_pos = tip * 1.1  # Adjust y position based on max histogram height
+        ax.text(x_pos, y_pos, stats_text, verticalalignment='top', horizontalalignment='left', 
+                bbox=dict(boxstyle="round", alpha=0.1, facecolor='white'))
+
+    # Set the y-axis limit to ensure space for text
+    ax.set_ylim(0, max_height * 1.15)  # Increase by 40% of the max height
+    
+    # Adjust the x-axis limit to ensure space for text annotations
+    ax.set_xlim(min(all_means)*0.8, max(all_means) * 1.2) 
+
+    # Set labels and title
+    ax.set_xlabel('Performance (microseconds)')
+    ax.set_ylabel('Density')
+    ax.set_title('Densities for 100 runs')
+    ax.legend()
+    
+    # Save the graph
+    plt.savefig(f"Analysis/{job}/density_log_{filename}.png")
+    plt.show()
+    print(f"File has been saved to {job}/density_log_{filename}.png")
+
+def coe_dist(coe_content, job, filename):
+    # Extract slope and interception values
+    slope = coe_content[:, 0]
+    interception = coe_content[:, 1]
+    std_error = coe_content[:, 2]
+
+    # Remove outliers based on standard error
+    outliers_mask = np.abs(zscore(std_error)) > 1.96  # Using 95% confidence interval
+    slope_filtered = slope[~outliers_mask]
+    interception_filtered = interception[~outliers_mask]
+    outliers_count = np.sum(outliers_mask)
+
+    # Plot the distributions of slopes and interceptions with histograms
+    slope_mean = np.mean(slope_filtered)
+    slope_median = np.median(slope_filtered)
+    slope_std_dev = np.std(slope_filtered)
+    interception_mean = np.mean(interception_filtered)
+    interception_median = np.median(interception_filtered)
+    interception_std_dev = np.std(interception_filtered)
+
+     # Box plot for slopes and interceptions
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6), tight_layout=True)
+
+    axs[0].text(0.95, 0.9, f"Mean: {slope_mean:.6f}\nMedian: {slope_median:.6f}\nStd Dev: {slope_std_dev:.6f}",
+            transform=axs[0].transAxes, ha='right', va='top', bbox=dict(facecolor='white', alpha=0.5))
+
+    axs[1].text(0.95, 0.8, f"Mean: {interception_mean:.6f}\nMedian: {interception_median:.6f}\nStd Dev: {interception_std_dev:.6f}",
+            transform=axs[1].transAxes, ha='right', va='top', bbox=dict(facecolor='white', alpha=0.5))
+
+    axs[0].boxplot(slope_filtered, notch=True, vert=True, patch_artist=True)
+    axs[0].set_title('Box Plot of Slopes')
+    axs[0].set_ylabel('Slope Value')
+
+    axs[1].boxplot(interception_filtered, notch=True, vert=True, patch_artist=True)
+    axs[1].set_title('Box Plot of Interceptions')
+    axs[1].set_ylabel('Interception Value')
+
+    plt.savefig(f"Analysis/{job}/distribution_log_{filename}.png")
+    plt.show()
+    print(f"File has been saved to {job}/distribution_log_{filename}.png")
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Parse the multiple runs results and analysis the distribution.')
+    parser.add_argument('data_filename', type=str, help='Name of the data file to parse')
+    parser.add_argument('coe_filename', type=str, help='Name of the coeficient to parse')
+    parser.add_argument('job',type=str, help='The output diagrams directory')
+
+    args = parser.parse_args()
+    # Extract 'filename' from the input file name using string methods
+    parts = args.data_filename.split('_')
+    filename = '_'.join(parts[1:])  # This joins all parts between 'data' and '27.txt'
+    print(filename)
+
+    data_content = np.transpose(np.loadtxt(args.data_filename))
+    coe_content = np.loadtxt(args.coe_filename)
+
+    runs_dist(data_content, args.job, filename)
+    coe_dist(coe_content, args.job, filename)
+
+if __name__ == "__main__":
+    main()
--- openmpbench_C_v40/visualise.py
+++ openmpbench_C_v40/visualise.py
@@ -15,151 +15,106 @@ def read_file(filename):
         content = file.read()
     return content
 
-def plot_performance(benchmark_data, delay_times, plot_filename):
+def plot_performance(benchmark_data, delays, job, IDA, graph):
     # Plotting performance based on extracted data
     plt.figure(figsize=(10, 6))
     colors = iter(['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2'])
-    extended_x = np.linspace(0, max(delay_times) * 1.1, 100)
+    extended_x = np.linspace(0, max(delays) * 1.1, 100)
 
     for benchmark, data in benchmark_data.items():
         color = next(colors)
-        slope, intercept, r_value, p_value, std_err = linregress(delay_times, data)
+        slope, intercept, r_value, p_value, std_err = linregress(delays, data)
+        analyse_dist(benchmark, job, slope, intercept, std_err)
         extended_predictions = intercept + slope * extended_x
 
         plt.plot(extended_x, extended_predictions, color=color, linestyle='-', linewidth=2, label=f'{benchmark} (y = {slope:.4f}x + {intercept:.4f})')
-        plt.scatter(delay_times, data, color=color, edgecolor='black', s=50)
+        plt.scatter(delays, data, color=color, edgecolor='black', s=50)
 
-    plt.title('Microbenchmarking OpenMP Target Offloading', fontsize=16, fontweight='bold')
-    plt.xlabel('Delay Time (microseconds)', fontsize=14)
-    plt.ylabel('Mean execution time (microseconds)', fontsize=14)
-    plt.legend(fontsize=12)
-    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
-    plt.minorticks_on()
-    plt.tick_params(axis='both', which='major', labelsize=12)
-    plt.xlim(left=0)
-    plt.ylim(bottom=0)
-    plt.tight_layout()
-    plt.savefig(plot_filename)
-    plt.show()
-    print(f"Plot saved to {plot_filename}")
+    if (graph==True):
+        plt.title('Microbenchmarking OpenMP Target Offloading', fontsize=16, fontweight='bold')
+        plt.xlabel('Delay Time (microseconds)', fontsize=14)
+        plt.ylabel('Mean execution time (microseconds)', fontsize=14)
+        plt.legend(fontsize=12)
+        plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+        plt.minorticks_on()
+        plt.tick_params(axis='both', which='major', labelsize=12)
+        plt.xlim(left=0)
+        plt.ylim(bottom=0)
+        plt.tight_layout()
+        output_name = "Plots/" + job + "/arraybench_" + IDA + "_performance.png"
+        plt.savefig(output_name)
+        plt.show()
+        print(f"Plot saved to {output_name}")
 
-# def plot_overhead(benchmark_overhead, delay_times, plot_filename):
-#     # Plotting performance based on extracted data
-#     plt.figure(figsize=(10, 6))
-#     colors = iter(['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2'])
-#     extended_x = np.linspace(0, max(delay_times) * 1.1, 100)
 
-#     for benchmark, data in benchmark_overhead.items():
-#         color = next(colors)
-#         slope, intercept, r_value, p_value, std_err = linregress(delay_times, data)
-#         extended_predictions = intercept + slope * extended_x
-
-#         plt.plot(extended_x, extended_predictions, color=color, linestyle='-', linewidth=2, label=f'{benchmark} (y = {slope:.4f}x + {intercept:.4f})')
-#         plt.scatter(delay_times, data, color=color, edgecolor='black', s=50)
-
-#     plt.title('Microbenchmarking OpenMP Target Offloading', fontsize=16, fontweight='bold')
-#     plt.xlabel('Delay Time (microseconds)', fontsize=14)
-#     plt.ylabel('Overhead (microseconds)', fontsize=14)
-#     plt.legend(fontsize=12)
-#     plt.grid(True, which='both', linestyle='--', linewidth=0.5)
-#     plt.minorticks_on()
-#     plt.tick_params(axis='both', which='major', labelsize=12)
-#     plt.xlim(left=0)
-#     plt.ylim(bottom=0)
-#     plt.tight_layout()
-#     plt.savefig(plot_filename)
-#     plt.show()
-#     print(f"Plot saved to {plot_filename}")
-    
-def plot_overhead(benchmark_overhead, delay_times, plot_filename):
+def plot_overhead(benchmark_overhead, delays, job, IDA, graph):
     # Plotting performance based on extracted data
     plt.figure(figsize=(10, 6))
     colors = itertools.cycle(['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2'])
-    extended_x = np.linspace(0, max(delay_times) * 1.1, 100)
-    
-    
-    # for benchmark, data in benchmark_overhead.items():
-    #     best_fit_degree = 0
-    #     best_fit_aic = float('inf')
-    #     color = next(colors)
-    #     for degree in range(0, 4):  # Try polynomial degrees 1, 2, 3
-    #         # Fit a polynomial of the current degree to the data
-    #         coefficients = np.polyfit(delay_times, data, degree)
-    #         polynomial = np.poly1d(coefficients)
-    #         # extended_predictions = polynomial(extended_x)
+    extended_x = np.linspace(0, max(delays) * 1.1, 100)
 
-    #         # Calculate AIC for the current fit
-    #         residuals = data - polynomial(delay_times)
-    #         aic = len(data) * np.log(np.mean(residuals**2)) + 2 * degree
-
-    #         if aic < best_fit_aic:
-    #             best_fit_degree = degree
-    #             best_fit_aic = aic
-    #             best_polynomial = polynomial
-    #             best_color = color
-
-    #     # Plot the best-fit polynomial
-    #     plt.plot(extended_x, best_polynomial(extended_x), color=best_color, linestyle='-', linewidth=2, label=f'Best({best_fit_degree}): {best_polynomial}')
-    #     # for benchmark, data in benchmark_overhead.items():
-    #     plt.scatter(delay_times, data, color=color, edgecolor='black', s=50)
-
-    #     # Extract the constant term for the lower limit horizontal line y = a
-    #     lower_limit_constant = best_polynomial(extended_x)[-1]
-
-    #     # Plot the lower limit horizontal line
-    #     plt.axhline(y=lower_limit_constant, color='gray', linestyle='--', label=f'Lower Limit (y = {lower_limit_constant:.2f})')
     for benchmark, data in benchmark_overhead.items():
-        color = next(colors)
-        
+        color = next(colors)       
         # Use UnivariateSpline for smoothing. s is a smoothing factor.
-        spline = UnivariateSpline(delay_times, data, s=1)
-        
+        spline = UnivariateSpline(delays, data, s=1)        
         # Plot the spline fit
         plt.plot(extended_x, spline(extended_x), color=color, linestyle='-', linewidth=2, label=f'{benchmark}')
-        plt.scatter(delay_times, data, color=color, edgecolor='black', s=50)
-        
+        plt.scatter(delays, data, color=color, edgecolor='black', s=50)    
         # Determine the lower limit constant 'a' as the minimum of the smoothed data
         lower_limit_constant = np.min(spline(extended_x))
-
         # Plot the lower limit horizontal line
         plt.axhline(y=lower_limit_constant, color='gray', linestyle='--', label=f'Lower Limit (y = {lower_limit_constant:.2f})')
 
+    if(graph==True):
+        plt.title('Microbenchmarking OpenMP Target Offloading', fontsize=16, fontweight='bold')
+        plt.xlabel('Delay Time (microseconds)', fontsize=14)
+        plt.ylabel('Overhead (microseconds)', fontsize=14)
+        plt.legend(fontsize=12)
+        plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+        plt.minorticks_on()
+        plt.tick_params(axis='both', which='major', labelsize=12)
+        plt.xlim(left=0)
+        plt.ylim(bottom=0)
+        plt.tight_layout()
+        output_name = "Plots/" + job + "/arraybench_" + IDA + "_overhead.png"
+        plt.savefig(output_name)
+        plt.show()
+        print(f"Plot saved to {output_name}")
 
-    plt.title('Microbenchmarking OpenMP Target Offloading', fontsize=16, fontweight='bold')
-    plt.xlabel('Delay Time (microseconds)', fontsize=14)
-    plt.ylabel('Overhead (microseconds)', fontsize=14)
-    plt.legend(fontsize=12)
-    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
-    plt.minorticks_on()
-    plt.tick_params(axis='both', which='major', labelsize=12)
-    plt.xlim(left=0)
-    plt.ylim(bottom=0)
-    plt.tight_layout()
-    plt.savefig(plot_filename)
-    plt.show()
-    print(f"Plot saved to {plot_filename}")
+def analyse_data(benchmark_name, job, data):
+    file_name = "Analysis/" + job + "/data_" + benchmark_name + ".txt"
+    with open(file_name, 'w') as data_file:
+        for item in data:
+            data_file.write(str(item) + ' ') 
+        data_file.write('\n')
 
+def analyse_dist(benchmark_name, job, gradient, interception, error):
+    file_name = "Analysis/" + job + "/coe_" + benchmark_name + ".txt"
+    with open(file_name, 'w') as coe_file:
+        coe_file.write(f"{gradient} {interception} {error}\n")  # Using f-string for formatting
 
 def main():
     # Setting up argument parser
     parser = argparse.ArgumentParser(description='Parse and plot data from OpenMP benchmark output.')
     parser.add_argument('filename', type=str, help='Name of the output file to parse')
-    parser.add_argument('output_dir', type=str, default='Plots', help='Directory to save the plot')
+    parser.add_argument('IDA', type=str, default='1', help='Array Length')
+    parser.add_argument('job',type=str,default='0',help='The Script JOB ID')
+
     args = parser.parse_args()
-
     content = read_file(args.filename)
-
+    job = args.job
+    IDA = args.IDA
     # Extract parameters
     param_pattern = r"Running OpenMP benchmark version \d+\.\d+\s+(\d+) thread\(s\)\s+(\d+) outer repetitions\s+([\d.]+) test time \(microseconds\)\s+(\d+) delay length \(iterations\)\s+([\d.]+) delay time \(microseconds\)"
     params = re.findall(param_pattern, content, re.DOTALL)
 
-    delay_times = []
+    delays = []
     benchmark_data = {}
     benchmark_overhead = {}
 
     if params:
         for i, (threads, outer_repetitions, test_time, delay_length, delay_time) in enumerate(params, start=1):
-            delay_times.append(float(delay_time))
+            delays.append(float(delay_time))
             # print(f"Run {i}: Threads: {threads}, Outer Repetitions: {outer_repetitions}, Test Time: {test_time} microseconds, Delay Length: {delay_length}, Delay Time: {delay_time} microseconds")
     else:
         print("Parameters not found in the file.")
@@ -190,6 +145,7 @@ def main():
             benchmark_data[benchmark].append(float(mean_time))
         for benchmark, mean_times in benchmark_data.items():
             print(f"{benchmark}: {mean_times}")
+            analyse_data(benchmark, job, mean_times)
     else:
         print("Data not found in the file.")
         exit()    
@@ -210,9 +166,8 @@ def main():
         exit()        
     
 
-    # Ensure the output directory exists
-    plot_performance(benchmark_data, delay_times, args.output_dir+'_performance_plot.png')
-    plot_overhead(benchmark_overhead, delay_times, args.output_dir+'_overhead_plot.png')
+    plot_performance(benchmark_data, delays, job, IDA, True)
+    plot_overhead(benchmark_overhead, delays, job, IDA, True)
 
 
 if __name__ == "__main__":
